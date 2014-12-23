635007a1caa06aae8cfedf4cb323451a9488afc8
Herman Kopinga <herman@kopinga.nl>
2014-12-23 11:50:18 +0100

    Teensy 3.1 and 2 line implementation HACK.
    Migrated to Teensy 3.1 from Arduino UNO.
    Bugfix in printChar when using passivebuffer.
    Hacked 2 line implementation, temporary (I hope)

--- willemeen.ino
+++ willemeen.ino
@@ -14,9 +14,13 @@
 
 #include "font.h"
 
-int latchPin = 10;
-int clockPin = 11;
-int dataPin = 12;
+int latchPin1 = 9;
+int clockPin1 = 13;
+int dataPin1 = 11;
+int latchPin2 = 10;
+int clockPin2 = 14;
+int dataPin2 = 7;
+
 
 unsigned long screenbuffer[2][7][5];
 
@@ -30,53 +34,49 @@ byte activebuffer = 0;
 byte passivebuffer = 1;
 
 void setup() {
- pinMode(latchPin, OUTPUT);
- pinMode(clockPin, OUTPUT);
- pinMode(dataPin, OUTPUT);
- pinMode(13, OUTPUT);
- pinMode(14, OUTPUT);
- pinMode(15, OUTPUT);
- pinMode(16, OUTPUT);
- pinMode(17, OUTPUT);
- pinMode(18, OUTPUT);
- pinMode(19, OUTPUT); 
- digitalWrite(latchPin, 0); //make sure data isn't latched
- digitalWrite(13, 1);
- digitalWrite(14, 1);
- digitalWrite(15, 1);
- digitalWrite(16, 1);
- digitalWrite(17, 1);
- digitalWrite(18, 1);
- digitalWrite(19, 1);
- Serial.begin(115200);
- /*
-  screenbuffer[activebuffer][0][0] = 0xAAAAAAAA;
-  screenbuffer[activebuffer][1][0] = 0x55555555;
-  screenbuffer[activebuffer][2][0] = 0xAAAAAAAA;
-  screenbuffer[activebuffer][3][0] = 0x55555555;
-  screenbuffer[activebuffer][4][0] = 0xAAAAAAAA;
-  screenbuffer[activebuffer][5][0] = 0x55555555;
-  screenbuffer[activebuffer][6][0] = 0xAAAAAAAA;
-/*
-screenbuffer[activebuffer][0][0] = 0x10011;
-screenbuffer[activebuffer][1][0] = 0x1001B;
-screenbuffer[activebuffer][2][0] = 0x939D5;
-screenbuffer[activebuffer][3][0] = 0x54211;
-screenbuffer[activebuffer][4][0] = 0x37BD1;
-screenbuffer[activebuffer][5][0] = 0x54631;
-screenbuffer[activebuffer][6][0] = 0x97BD1;*/
- /* printChar('M',0);
-  printChar('a',5);
-  printChar('a',10);
-  printChar('k',15);
-  printChar('p',20);
-  printChar('l',25);
-  printChar('e',30);  
-  printChar('k',35);    
-  printChar('j',40);    
-  printChar('e',45);    */ 
-  char tekst[29] = "Maakplek";
-  printString(tekst, 0, activebuffer);
+  pinMode(latchPin1, OUTPUT);
+  pinMode(clockPin1, OUTPUT);
+  pinMode(dataPin1, OUTPUT);
+  pinMode(latchPin2, OUTPUT);
+  pinMode(clockPin2, OUTPUT);
+  pinMode(dataPin2, OUTPUT);
+  pinMode(17, OUTPUT);
+  pinMode(18, OUTPUT);
+  pinMode(19, OUTPUT);
+  pinMode(20, OUTPUT);
+  pinMode(21, OUTPUT);
+  pinMode(22, OUTPUT);
+  pinMode(23, OUTPUT); 
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT); 
+ 
+  digitalWrite(latchPin1, 0); //make sure data isn't latched
+  digitalWrite(latchPin2, 0); //make sure data isn't latched
+  digitalWrite(17, 1);
+  digitalWrite(18, 1);
+  digitalWrite(19, 1);
+  digitalWrite(20, 1);
+  digitalWrite(21, 1);
+  digitalWrite(22, 1);
+  digitalWrite(23, 1);
+  digitalWrite(0, 1);
+  digitalWrite(1, 1);
+  digitalWrite(2, 1);
+  digitalWrite(3, 1);
+  digitalWrite(4, 1);
+  digitalWrite(5, 1);
+  digitalWrite(6, 1);
+  Serial.begin(115200);
+
+  char text[29] = "     Dank je wel Pim!";
+  char text2[29] = " Dank jullie wel Willemeen!";
+  printString(text, 0, activebuffer);
+  printString(text2, 0, passivebuffer);
 }
 
 void loop() {
@@ -87,7 +87,6 @@ void loop() {
     previousMillis = currentMillis;   
     // if the LED is off turn it on and vice-versa:
     //updatebuffer();
-
   }  
   updatescreen();
 
@@ -112,35 +111,38 @@ void communicate () {
 void updatescreen() {
   // 7 rows
   for (int i=0 ; i < 7; i++) {
-
-    
-/*    shiftOut(dataPin, clockPin, LSBFIRST, screenbuffer[activebuffer][i][1]);    
-    shiftOut(dataPin, clockPin, LSBFIRST, screenbuffer[activebuffer][i][0]); 
-    shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][0] >> 8)); 
-    shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][0] >> 16));
-    shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][0] >> 24)); */
     
     // Update row.
     for (int j = 5; j >= 0; j--) {
-      shiftOut(dataPin, clockPin, LSBFIRST, screenbuffer[activebuffer][i][j]); 
-      shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 8)); 
-      shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 16));
-      shiftOut(dataPin, clockPin, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 24)); 
+      shiftOut(dataPin1, clockPin1, LSBFIRST, screenbuffer[activebuffer][i][j]); 
+      shiftOut(dataPin1, clockPin1, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 8)); 
+      shiftOut(dataPin1, clockPin1, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 16));
+      shiftOut(dataPin1, clockPin1, LSBFIRST, (screenbuffer[activebuffer][i][j] >> 24)); 
+
+      shiftOut(dataPin2, clockPin2, LSBFIRST, screenbuffer[passivebuffer][i][j]); 
+      shiftOut(dataPin2, clockPin2, LSBFIRST, (screenbuffer[passivebuffer][i][j] >> 8)); 
+      shiftOut(dataPin2, clockPin2, LSBFIRST, (screenbuffer[passivebuffer][i][j] >> 16));
+      shiftOut(dataPin2, clockPin2, LSBFIRST, (screenbuffer[passivebuffer][i][j] >> 24));       
     }
 
-    // Turn off row.
+    // Turn off last row.
     if (i == 0) {
-      digitalWrite(19, 1);
+      digitalWrite(23, 1);
+      digitalWrite(6, 1);      
     }
     else {   
-      digitalWrite(i + 12, 1);
+      digitalWrite(i + 16, 1);
+      digitalWrite(i - 1, 1);      
     }    
     // Flick the latch
-    digitalWrite(latchPin, 1);
-  //  delay(1);
-    digitalWrite(latchPin, 0);
-    // Turn on next row.
-    digitalWrite(i + 13, 0);    
+    digitalWrite(latchPin1, 1);
+    digitalWrite(latchPin2, 1);
+    //delay(1);
+    digitalWrite(latchPin1, 0);
+    digitalWrite(latchPin2, 0);
+    // Turn on new row.
+    digitalWrite(i + 17, 0);    
+    digitalWrite(i, 0);    
   }
 }
 
@@ -161,8 +163,8 @@ void printChar(char ch, byte charPos, byte bufferpointer)
   for (int charCol=0; charCol<5; charCol++) {
     byte b = font[ch][charCol];
 
-    bufferpointer = (charCol+charPos)/32;
-    bufferpos = charPos - (32 * bufferpointer);
+    byte bufferbyte = (charCol+charPos)/32;
+    bufferpos = charPos - (32 * bufferbyte);
         
     //  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39
     // 31 30 29 28 27 26 25 24 23 22 21 20 19 18 17 16 15 14 13 12 11 10  9  8  7  6  5  4  3  2  1  0 
@@ -173,16 +175,16 @@ void printChar(char ch, byte charPos, byte bufferpointer)
     // bit shift through the byte, set it in the buffer
     for (int row=0; row<7; row++) {
       if (b & (1 << row)) {        
-        bitSet(screenbuffer[activebuffer][row][bufferpointer], 31-charCol-bufferpos);
+        bitSet(screenbuffer[bufferpointer][row][bufferbyte], 31-charCol-bufferpos);
       }
       else {
-        bitClear(screenbuffer[activebuffer][row][bufferpointer], 31-charCol-bufferpos);
+        bitClear(screenbuffer[bufferpointer][row][bufferbyte], 31-charCol-bufferpos);
       }
     }
   }
 }
 
-void printString(char* source, byte pos, char bufferpointer) {
+void printString(char* source, byte pos, byte bufferpointer) {
   int i;
   for (i = 0; i < strlen(source); i++){
     printChar(source[i], pos+i*5, bufferpointer);
